8c1071ae88a814c846ddf5dfeff31328c100bd8a
Nicholas Corgan <n.corgan@gmail.com>
2015-10-04 20:19:34 -0700

    praxyk::get_string_from_image works in both C++ and Python

--- server/utils/praxyk-util1.cpp
+++ server/utils/praxyk-util1.cpp
@@ -1,14 +1,10 @@
-#include <praxyk/src1.hpp>
+#include <praxyk/praxyk.hpp>
 
 #include <cstdlib>
 #include <iostream>
 
-int main() {
-    int praxyk_int = praxyk::return_int();
-    std::string praxyk_string = praxyk::return_string();
-
-    std::cout << "praxyk_int = " << praxyk_int << std::endl;
-    std::cout << "praxyk_string = " << praxyk_string << std::endl;
+int main(int argc, char* argv[]) {
+    std::cout << "string from image: " << praxyk::get_string_from_image(argv[1]) << std::endl;
 
     return EXIT_SUCCESS;
 }
