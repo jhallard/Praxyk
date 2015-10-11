7b45b90702fbcd40c8c30909aa97956ac2acc436
Nicholas Corgan <n.corgan@gmail.com>
2015-10-10 22:04:05 -0700

    cmake: don't require TIFF

--- pod/cmake/Modules/FindTesseract.cmake
+++ pod/cmake/Modules/FindTesseract.cmake
@@ -14,11 +14,6 @@ find_library(Tesseract_LIBRARIES NAMES tesseract_full tesseract_api tesseract
   /usr/lib
   /usr/local/lib)
 
-find_library(Tiff_LIBRARY NAMES tiff
-  HINTS
-  /usr/lib
-  /usr/local/lib)
-
 if(BUILD_STATIC)
 # -llept -lgif -lwebp -ltiff -lpng -ljpeg -lz
 find_library(Lept_LIBRARY NAMES lept
@@ -34,7 +29,6 @@ find_library(Webp_LIBRARY NAMES webp
 find_package(GIF)
 find_package(JPEG)
 find_package(PNG)
-#find_package(TIFF) TODO replace manual find_library call
 find_package(ZLIB)
 
 endif()
@@ -61,7 +55,7 @@ list(REMOVE_ITEM CMAKE_REQUIRED_INCLUDES ${Tesseract_INCLUDE_DIR})
 if(BUILD_STATIC)
   set(Tesseract_LIBRARIES ${Tesseract_LIBRARIES} ${Lept_LIBRARY} ${PNG_LIBRARY} ${Tiff_LIBRARY} ${Webp_LIBRARY} ${GIF_LIBRARY} ${JPEG_LIBRARY} ${ZLIB_LIBRARY})
 else()
-  set(Tesseract_LIBRARIES ${Tesseract_LIBRARIES} ${Tiff_LIBRARY})
+  set(Tesseract_LIBRARIES ${Tesseract_LIBRARIES})
 endif()
 
 include(FindPackageHandleStandardArgs)
