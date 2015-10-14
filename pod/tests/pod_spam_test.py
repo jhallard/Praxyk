f7050e7290471435f49fc50a2baaddcfb5d1590b
Nicholas Corgan <n.corgan@gmail.com>
2015-10-14 15:17:52 -0700

    Added skeleton spam POD test

---
+++ pod/tests/pod_spam_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import praxyk
+
+import sys
+import traceback
+
+def spam_test1():
+    try:
+        return True
+    except:
+        print
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback,
+                                  limit=5, file=sys.stdout)
+        return False
+
+def spam_test2():
+    try:
+        return True
+    except:
+        print
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback,
+                                  limit=5, file=sys.stdout)
+        return False
+
+def spam_test3():
+    try:
+        return True
+    except:
+        print
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        traceback.print_exception(exc_type, exc_value, exc_traceback,
+                                  limit=5, file=sys.stdout)
+        return False
+
+if __name__ == "__main__":
+    successful = spam_test1() and spam_test2() and spam_test3()
+    sys.exit(0 if successful else 1)
