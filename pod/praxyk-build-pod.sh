64e3bd96258775f0c3df54a24ef6e02a00d49495
Nicholas Corgan <n.corgan@gmail.com>
2015-10-20 22:05:29 -0700

    pod: updated build script with MLPack dependencies

--- pod/praxyk-build-pod.sh
+++ pod/praxyk-build-pod.sh
@@ -5,15 +5,22 @@ export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 
 cd $HOME
-sudo apt-get install clang automake libtool cmake swig python-dev libleptonica-dev libfann-dev libicu-dev libpango1.0-dev libcairo2-dev libboost-thread-dev
+sudo apt-get install libboost-math-dev libboost-program-options-dev libboost-random-dev \
+libboost-test-dev libxml2-dev libarmadillo-dev automake libtool cmake swig python-dev \
+libleptonica-dev libfann-dev libicu-dev libpango1.0-dev libcairo2-dev libboost-thread-dev
 git clone https://github.com/tesseract-ocr/tesseract -b 3.02.02
 cd tesseract
 ./autogen.sh
 ./configure
-sudo make -j2 install
+sudo make install
 cd $HOME
 git clone https://github.com/tesseract-ocr/tessdata
-sudo cp -rv tessdata/* /usr/local/share/tessdata
+sudo cp -r tessdata/* /usr/local/share/tessdata
+git clone https://github.com/mlpack/mlpack.git -b mlpack-1.0.12
+mkdir mlpack/build
+cd mlpack/build
+cmake ..
+sudo make install
 
 echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> $HOME/.bashrc
 sudo ldconfig
