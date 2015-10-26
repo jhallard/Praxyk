020410a7f64ab0608b8ede09dea5b500ebee58bc
Nicholas Corgan <n.corgan@gmail.com>
2015-10-25 20:09:58 -0700

    Starting facial detection

--- pod/lib/facial_rec.cpp
+++ pod/lib/facial_rec.cpp
@@ -1,9 +1,17 @@
 #include <praxyk/facial_rec.hpp>
 
+#include "clandmark/Flandmark.h"
+
 namespace praxyk {
     face_boxes_t detect_faces_in_image(
         const std::string &filename
     ) {
-        return face_boxes_t();
+        face_boxes_t ret;
+
+        clandmark::Flandmark* flandmark = clandmark::Flandmark::getInstanceOf(
+                                               filename.c_str(), false
+                                          );
+
+        return ret;
     }
 }
