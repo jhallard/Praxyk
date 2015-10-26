e944b97ddeabb958279180ab0bc4656f43371f03
Nicholas Corgan <n.corgan@gmail.com>
2015-10-25 19:50:40 -0700

    Brought in Landmark source

--- pod/praxyk-build-pod.sh
+++ pod/praxyk-build-pod.sh
@@ -7,7 +7,8 @@ export CXX=/usr/bin/clang++
 cd $HOME
 sudo apt-get install libboost-math-dev libboost-program-options-dev libboost-random-dev \
 libboost-test-dev libxml2-dev libarmadillo-dev automake libtool cmake swig python-dev \
-libleptonica-dev libfann-dev libicu-dev libpango1.0-dev libcairo2-dev libboost-thread-dev
+libleptonica-dev libfann-dev libicu-dev libpango1.0-dev libcairo2-dev libboost-thread-dev \
+cimg-dev
 git clone https://github.com/tesseract-ocr/tesseract -b 3.02.02
 cd tesseract
 ./autogen.sh
