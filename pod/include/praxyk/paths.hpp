cb01ab9bd55ac8b96c64b10af5427f1176493fe4
Nicholas Corgan <n.corgan@gmail.com>
2015-10-25 19:55:10 -0700

    Brought in Landmark trained data

--- pod/include/praxyk/paths.hpp
+++ pod/include/praxyk/paths.hpp
@@ -7,6 +7,10 @@
 
 namespace praxyk {
     PRAXYK_API std::string get_pkg_data_dir();
+
+    PRAXYK_INLINE std::string get_landmark_dir() {
+        return (get_pkg_data_dir() + "/landmark");
+    }
 }
 
 #endif /* INCLUDED_PRAXYK_PATHS_HPP */
