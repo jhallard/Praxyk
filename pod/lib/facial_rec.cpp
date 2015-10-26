0147d91b602b25338a62286cbf74caac25ae9ac5
Nicholas Corgan <n.corgan@gmail.com>
2015-10-26 15:39:00 -0700

    More face rec work, may need to change models

--- pod/lib/facial_rec.cpp
+++ pod/lib/facial_rec.cpp
@@ -8,10 +8,26 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
 
+#include <iostream>
 #include <stdexcept>
 #include <vector>
 
 namespace praxyk {
+    static cimg_library::CImg<unsigned char>* cvImg_to_CImg(
+        const cv::Mat &cvImg
+    ) {
+        cimg_library::CImg<unsigned char>* result = new cimg_library::CImg<unsigned char>(
+                                                            cvImg.cols, cvImg.rows
+                                                        );
+        for(int i = 0; i < cvImg.cols; ++i) {
+            for(int j = 0; j < cvImg.rows; ++j) {
+                (*result)(i,j) = cvImg.at<uchar>(j,i);
+            }
+        }
+
+        return result;
+    }
+
     face_boxes_t detect_faces_in_image(
         const std::string &filename
     ) {
@@ -67,6 +83,21 @@ namespace praxyk {
             1.1, 2, (0 | CV_HAAR_SCALE_IMAGE),
             cv::Size(30, 30)
         );
+        std::cout << "Faces detected: " << faces.size() << std::endl;
+        for(size_t i = 0; i < faces.size(); i++) {
+            face_box_t face_box;
+            // We already have the head itself, so put it in result
+            face_box.head.x      = faces[i].x;
+            face_box.head.y      = faces[i].y;
+            face_box.head.width  = faces[i].width;
+            face_box.head.height = faces[i].height;
+
+            cimg_library::CImg<unsigned char>* cimg_gray = cvImg_to_CImg(image_gray);
+            flandmark->detect_optimized(cimg_gray, bbox);
+
+            ret.push_back(face_box);
+            delete cimg_gray;
+        }
 
         /*
          * Cleanup
