d6fc18b34cfc4659f0b9b0905b818fb117c130c2
jhallard <john@jhallard.com>
2015-11-03 02:49:20 -0800

    More trvis stuff

--- api/api_build.sh
+++ api/api_build.sh
@@ -1,8 +1,8 @@
-cat api/.ubuntu_install | xargs sudo apt-get -y install
+cat api/.ubuntu_install | xargs sudo apt-get -y install > .build.log
 RETVAL=$?
 [ $RETVAL -eq 0 ] && echo API Ubuntu Requirements Install Success
 [ $RETVAL -ne 0 ] && echo API Ubuntu Requirements Install Failure && echo 1
-sudo pip install -r api/.pip_install
+sudo pip install -r api/.pip_install > .build.log
 RETVAL=$?
 [ $RETVAL -eq 0 ] && echo API Pip Requirements Install Success
 [ $RETVAL -ne 0 ] && echo API Pip Requirements Install Failure && echo 1
--- build.sh
+++ build.sh
@@ -3,15 +3,14 @@ echo "Beginning Praxyk Build Process"
 cp -R .praxyk/ ~/.praxyk_travis # move the fake config files to the home directory
 ln -s ~/.praxyk_travis ~/.praxyk
 
-declare -a arr=("api") # "models" "queue" "website" "pod" "devops" "docs")
+declare -a arr=("api" "pod") # "models" "queue" "website" "pod" "devops" "docs")
 
 sudo apt-get install -y git python-pip build-essential
 
 for i in "${arr[@]}"
 do
     echo Starting "$i" Build Process
-    ./"$i"/"$i"_build.sh
-    RETVAL=$?
-    [ $RETVAL -eq 0 ] && echo "$i" Build Success
-    [ $RETVAL -ne 0 ] && echo "$i" Build Failure && echo 1
+    RETVAL=$(./"$i"/"$i"_build.sh)
+    [ $RETVAL -eq 0 ] && echo "$i" Build Success && rm "$i"/.build.log
+    [ $RETVAL -ne 0 ] && echo "$i" Build Failure && echo 1 && cat "$i"/.build.log
 done
