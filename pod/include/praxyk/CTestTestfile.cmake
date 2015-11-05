58bed900b4682abe3012f728efbfd9950a8aa1a0
Nicholas Corgan <n.corgan@gmail.com>
2015-11-05 13:41:15 -0800

    Removed CMake artifacts

--- pod/include/praxyk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jhallard/linux/praxyk-api/pod/include/praxyk
-# Build directory: /home/jhallard/linux/praxyk-api/pod/include/praxyk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- pod/python/praxyk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jhallard/linux/praxyk-api/pod/python/praxyk
-# Build directory: /home/jhallard/linux/praxyk-api/pod/python/praxyk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
