3204260878704aca9f1179e201ec08ddd26f667b
jhallard <john@jhallard.com>
2015-11-05 15:22:38 -0800

    Tried to  fix recursive clone bug involving the wiki.

--- docs/wiki/update_wiki.sh
+++ docs/wiki/update_wiki.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone https://github.com/jhallard/Praxyk.wiki.git praxyk_wiki_md
+git clone https://github.com/Praxyk/Praxyk.wiki.git praxyk_wiki_md
 generate-md --input praxyk_wiki_md --output praxyk_wiki_html
 
 
