0f66c2519801c257e1f20fbb6e9c9b5595b0a0a3
Michael Vincent <tekgek@gmail.com>
2015-11-19 14:30:20 -0800

    Build script will now send emails

--- daemon/container/build_master.sh
+++ daemon/container/build_master.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 ./docker_config.py --worker_config config.py --branch master
-./build.sh
+time ./build.sh > >(tee -i praxyk_log.txt)
 if [ $? != 0 ]; then
   echo "Error: docker build command failed."
   exit 1
+  mail -s "Praxyk master build - FAIL" mvincent@praxyk.com << praxyk_log.txt
 fi
-
 docker push tekgek/praxyk:latest
+mail -s "Praxyk master build - PASS" mvincent@praxyk.com << praxyk_log.txt
