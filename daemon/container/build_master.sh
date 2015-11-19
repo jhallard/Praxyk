57e21ee044c7d8c6000020e2802571662b5c9182
mvincent <mvincent@praxyk.com>
2015-11-19 17:50:17 -0500

    Subject line of passing message changed to PASS from FAIL.  Derp.

--- daemon/container/build_master.sh
+++ daemon/container/build_master.sh
@@ -7,4 +7,4 @@ if [ $? != 0 ]; then
   cat praxyk_log.txt | mail -s "Praxyk master build - FAIL" mvincent@praxyk.com
 fi
 docker push tekgek/praxyk:latest
-cat praxyk_log.txt | mail -s "Praxyk master build - FAIL" mvincent@praxyk.com
+cat praxyk_log.txt | mail -s "Praxyk master build - PASS" mvincent@praxyk.com
